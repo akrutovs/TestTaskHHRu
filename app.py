68f8ee8c80e0bdee04c17440a483efd6932f762b
Aleksey <a.krutovs@ya.ru>
2022-06-10 23:18:51 +0300

    add color generator

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+import random
 app = Flask(__name__)
 
 # db model
@@ -10,7 +10,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'loooong_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
+P = [69,19,12]
 
+#first logic
+def random_color_first(number,p):
+    while True:
+        a = number - p[2]
+        if a<0:
+            res = 'Красный'
+            break
+        a = a -  p[1]
+        if a <0:
+            res = 'Зеленый'
+            break
+        a = a - p[0]
+        if a < 0:
+            res = 'Синий'
+            break
+    return res
+
+def random_color_second(p):
+    res = random.choices(['Синий',"Зеленый", "Красный"], weights = p)
+    return res[0]
 
 def solver_eq(a, b, c):
     a = float(a)
@@ -64,6 +85,18 @@ def show_history():
     eq = Equation.query.order_by(Equation.registration_date.desc()).all()  # обращение к базе данных
     # передача списка в шаблон
     return render_template('history_page.html', eq=eq)
+
+@app.route('/random', methods=['POST', "GET"])
+def show_random():
+    if request.method == 'POST':
+        number = int(request.form['number'])
+        if number<=100:
+            color = random_color_first(number,P)
+        else:
+            color = "Введите число до 100"
+        return render_template('random_color.html', color=color)
+    else:
+        return render_template('random_color.html', color='')
 
 
 if __name__ == '__main__':
