b1e2ee5e65c3edd68c306b74702b60a27538d852
Aleksey <a.krutovs@ya.ru>
2022-06-10 17:17:39 +0300

    add templates

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
 
 # db model
@@ -10,9 +12,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return render_template('base.html')
 
+class equation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    a = db.Column(db.Float, nullable=False)
+    b = db.Column(db.Float, nullable=False)
+    c = db.Column(db.Float, nullable=False)
+    result = db.Column(db.Float, nullable=True)
+    registration_date = db.Column(db.DateTime, default=datetime.utcnow)
 
 if __name__ == '__main__':
     app.run()
